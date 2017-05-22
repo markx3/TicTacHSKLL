0c3376b561b436229ad18d669587be3b96945136
Marcos <marcos@eipper.com.br>
2017-05-22 17:43:13 -0300

    first commit

---
+++ tictactoe.hs
@@ -0,0 +1,7 @@
+data Jogador = O | X | V deriving (Eq, Show)
+type tabuleiro = [[Jogador]]
+
+-- Usar transposta p/ verificar colunas ~ Função transpose
+-- P/ diagonais (!!)
+-- Algoritmo Min Max p/ jogar contra computador
+
